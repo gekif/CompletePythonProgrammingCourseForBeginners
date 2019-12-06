3691b125cf6cdd65cfeafbcabc8e1913fcdb04f9
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:00:42 +0700

    2- Primitive Types: 5- Formatted Strings

---
+++ Variables/formattedString.py
@@ -0,0 +1,10 @@
+firstName = 'Dzulfikar'
+lastName = 'Maulana'
+
+full1 = firstName + " " + lastName
+full2 = f"{firstName} {lastName}"
+full3 = f"{len(firstName)} {2 + 2}"
+
+print(full1)
+print(full1)
+print(full3)
