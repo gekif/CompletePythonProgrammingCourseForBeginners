c62dbcea9c0c87bcbe8251eb99a135dffab1bbbc
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:07:22 +0700

    2- Primitive Types: 6- String Methods

---
+++ Variables/stringMethod.py
@@ -0,0 +1,23 @@
+# course = "python programming"
+course = " python programming"
+courseCapital = course.upper()
+# courseWithStrip = " python programming"
+
+print(course)
+print(course.upper())
+print(courseCapital)
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print(course.strip())
+print(course.rstrip())
+
+print(course.find("Pro"))
+
+print(course.replace("P", "j"))
+
+print("pro" in course)
+print("swift" not in course)
+
