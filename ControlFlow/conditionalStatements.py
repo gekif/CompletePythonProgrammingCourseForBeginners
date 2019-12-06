223ae9c6744a02420f232442869fb055f637052b
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:35:36 +0700

    3- Control Flow: 2- Conditional Statements

---
+++ ControlFlow/conditionalStatements.py
@@ -0,0 +1,14 @@
+# temperature = 35
+# temperature = 15
+# temperature = 15
+temperature = 5
+
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif  10 < temperature < 20:
+    print("It's nice")
+else:
+    print("It's cold")
+
+print("Done")
