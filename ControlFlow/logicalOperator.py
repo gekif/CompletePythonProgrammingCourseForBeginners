7d8183c58da053d5e519a8e2b809bd6f7df3dfa2
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:43:17 +0700

    3- Control Flow: 4- Logical Operators

---
+++ ControlFlow/logicalOperator.py
@@ -0,0 +1,20 @@
+"""
+and
+or
+not
+"""
+
+# highIncome = True
+highIncome = False
+goodCredit = True
+# student = True
+student = False
+
+# if highIncome and goodCredit:
+# if highIncome or goodCredit:
+# if not student:
+if (highIncome and goodCredit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+
