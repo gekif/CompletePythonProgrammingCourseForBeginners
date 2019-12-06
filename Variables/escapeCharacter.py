e63d8f8c3ed458e93aa82ef71050b14e6920f7b9
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 13:57:33 +0700

    2- Primitive Types: 4- Escape Sequences

---
+++ Variables/escapeCharacter.py
@@ -0,0 +1,14 @@
+# \"
+# \'
+# \\
+# \n
+
+course1 = "Python \"Programming"
+course2 = "Python \'Programming"
+course3 = "Python \\Programming"
+course4 = "Python \nProgramming"
+
+print(course1)
+print(course2)
+print(course3)
+print(course4)
