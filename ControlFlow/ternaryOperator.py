b86d23c984c32e6ef0b7afb4f5fc7b872a00ec0c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:39:22 +0700

    3- Control Flow: 3- Ternary Operator

---
+++ ControlFlow/ternaryOperator.py
@@ -0,0 +1,21 @@
+# age = 22
+age = 12
+
+"""
+if age >=18:
+    print("Eligible")
+else:
+    print("Not eligible")
+"""
+
+"""
+if age >=18:
+    message = "Eligible"
+else:
+    message = "Not eligible"
+"""
+
+# Ternary Operator
+message = "Eligible" if age >= 18 else "Not eligible"
+
+print(message)
