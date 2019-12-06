59361be7ddded871f00cc381024bcf8ee98a564c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 15:58:02 +0700

    3- Control Flow: 11- Iterables

---
+++ ControlFlow/iterableLoops.py
@@ -0,0 +1,23 @@
+"""
+for x in range(5):
+    print(x)
+"""
+
+"""
+print(type(5))
+print(type(range(5)))
+"""
+
+"""
+# Iterable
+# for x in "Pyhton":
+for x in [1, 2, 3, 4]:
+    print(x)
+"""
+
+shoppingChart = []
+
+for item in shoppingChart:
+    print(item)
+
+
