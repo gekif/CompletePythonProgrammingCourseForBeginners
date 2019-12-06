c778c1e48e5bc0bf044a9d462f5c20c1d17d308c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-06 14:53:24 +0700

    3- Control Flow: 8- For Loops

---
+++ ControlFlow/forLoops.py
@@ -0,0 +1,19 @@
+"""
+print("Sending a message")
+print("Sending a message")
+print("Sending a message")
+print("Sending a message")
+print("Sending a message")
+print("Sending a message")
+print("Sending a message")
+"""
+
+# for number in range(3):
+# for number in range(1, 4):
+for number in range(1, 10, 2):
+    # print("Attempt")
+    # print("Attempt", number)
+    # print("Attempt", number + 1)
+    # print("Attempt", number + 1, (number + 1) * ".")
+
+    print("Attempt", number, number  * ".")
