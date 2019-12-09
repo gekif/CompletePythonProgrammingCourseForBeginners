2a1e6e28809d8b92ffbf22f432a57e2b2750dbd2
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-09 10:17:11 +0700

    4- Functions: 2- Arguments

---
+++ Function/arguments.py
@@ -0,0 +1,7 @@
+def greet(firstName, lastName = "Doang"):
+    print(f"Hi there {firstName} {lastName}")
+    print("Welcome aboard")
+
+greet("Dzulfikar", "Maulana")
+greet("Febrina", "Pujihastuti")
+greet("Fregat")
