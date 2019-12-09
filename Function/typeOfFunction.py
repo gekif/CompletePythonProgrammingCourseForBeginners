d21e3affc19745a6e2ce12b1f8b9741b666b89a4
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-09 10:22:41 +0700

    4- Functions: 3- Types of Functions

---
+++ Function/typeOfFunction.py
@@ -0,0 +1,22 @@
+def greet(name):
+    # print(f"Hi {name}")
+    return f"Hi {name}"
+
+# 1 - Perform a task
+# 2 - Return a value
+
+# round(1.9)
+
+
+# def getGreeting(name):
+#     return f"Hi {name}"
+#
+# message = getGreeting("Fikar")
+# file = open("content.txt", "w")
+# file.write(message)
+
+# greet("Fikar")
+
+# Not work, only work on return
+print(greet("Fikar"))
+
